e1cd5d29cb70c0fb27d60e41aa4880949349cb05
Tyson Cleary <amec@tyscorp.net>
2016-06-05 15:55:10 -0500

    Update version.php

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014040801; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2014092601; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900; // Requires this Moodle version
 $plugin->component = 'theme_ifsta'; // Full name of the plugin (used for diagnostics)
